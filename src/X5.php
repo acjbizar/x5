2e8b18cce2c1fbefe37572c3f5f2d577479b9bf7
ACJ <gitlab@acjs.net>
2022-12-07 13:29:34 +0100

    Draw x

--- src/X5.php
+++ src/X5.php
@@ -18,14 +18,42 @@ class X5
         $this->chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
     }
 
-    private function _createImage()
+    private function _createImage(): void
     {
         $this->im = imagecreatetruecolor(1024, 1024);
+    }
+
+    private function _drawChar(): void
+    {
+        $f00 = imagecolorallocate($this->im, 255, 0, 0);
+        $l = $this->getChar();
+        $i = 0;
+        $x = 10;
+        $y = 10;
+
+        for($row = 1; $row <= 5; ++$row) {
+            for ($col = 1; $col <= 5; ++$col) {
+                if (isset($l[$i]) && $l[$i] === 1) {
+                    imagefilledrectangle($this->im, $x, $y, $x, $y, $f00);
+                }
+
+                ++$i;
+                $x += 1;
+            }
+
+            $x -= 5;
+            $y += 1;
+        }
     }
 
     public function dumpChars()
     {
         return $this->chars;
+    }
+
+    public function getChar(): array
+    {
+        return $this->chars[0x58];
     }
 
     public function getPower(): int
@@ -38,9 +66,10 @@ class X5
         $this->power = $power;
     }
 
-    public function parse()
+    public function parse(): void
     {
         $this->_createImage();
+        $this->_drawChar();
         header('Content-Type: image/png');
 
         imagepng($this->im);

3a55de38e73b2f032027c614951807b2d0d57ddb
ACJ <gitlab@acjs.net>
2022-12-08 13:57:21 +0100

    Add base characters

--- generator/chars.php
+++ generator/chars.php
@@ -6,7 +6,7 @@ require_once '../src/X5.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-for($i = 5; $i <= 5; $i++) {
+for($i = 1; $i <= 1; $i++) {
     foreach ($chars as $key => $value) {
         $x5 = new \Acj\X5\X5($key);
         $x5->setPower($i);

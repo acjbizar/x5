ea3b12fcc9aeef57be4f7882142db6ec1f8fb7d8
ACJ <gitlab@acjs.net>
2022-12-08 10:04:04 +0100

    Add larger images

--- generator/chars.php
+++ generator/chars.php
@@ -4,11 +4,13 @@ require_once '../src/X5.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-foreach ($chars as $key => $value) {
-    $x5 = new \Acj\X5\X5($key);
-    $x5->setPower(2);
+for($i = 2; $i <= 4; $i++) {
+    foreach ($chars as $key => $value) {
+        $x5 = new \Acj\X5\X5($key);
+        $x5->setPower($i);
 
-    if(isset($_GET['save'])) {
-        $x5->save();
+        if(isset($_GET['save'])) {
+            $x5->save();
+        }
     }
 }

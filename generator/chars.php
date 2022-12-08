8c67f5143dd608cfcde1b51c44a50e7540065475
ACJ <gitlab@acjs.net>
2022-12-08 10:48:55 +0100

    Build big characters

--- generator/chars.php
+++ generator/chars.php
@@ -1,10 +1,12 @@
 <?php
 
+set_time_limit(600);
+
 require_once '../src/X5.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-for($i = 2; $i <= 4; $i++) {
+for($i = 5; $i <= 5; $i++) {
     foreach ($chars as $key => $value) {
         $x5 = new \Acj\X5\X5($key);
         $x5->setPower($i);

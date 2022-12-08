d03cd82e98ea35412e152efa43c96f54d1d852f0
ACJ <gitlab@acjs.net>
2022-12-08 14:03:28 +0100

    Add base specials

--- generator/specials.php
+++ generator/specials.php
@@ -4,7 +4,7 @@ require_once '../src/X5.php';
 
 $specials = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'specials.php';
 
-for($i = 2; $i <= 5; $i++) {
+for($i = 1; $i <= 1; $i++) {
     foreach ($specials as $key => $value) {
         $x5 = new \Acj\X5\X5($key);
         $x5->setPower($i);

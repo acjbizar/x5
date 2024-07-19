56b360452c28a430cd98aebbef6275c2d9b7c318
ACJ <gitlab@acjs.net>
2024-07-19 16:15:39 +0200

    Add some characters

--- generator/char.php
+++ generator/char.php
@@ -6,7 +6,7 @@ require_once '../src/X5.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-$key = 0x25;
+$key = 0x2C;
 
 if(isset($_GET['save'])) {
 

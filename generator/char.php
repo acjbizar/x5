e9f863e2fab0f129e4c25af1a0ad2472fee69712
ACJ <gitlab@acjs.net>
2024-07-25 20:53:01 +0200

    Add some more characters

--- generator/char.php
+++ generator/char.php
@@ -6,14 +6,14 @@ require_once '../src/X5.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-$key = 0x20BF;
+$key = 'uni';
 
 if(isset($_GET['save'])) {
 
     for($i = 1; $i <= 5; $i++) {
         $x5 = new \Acj\X5\X5($key);
         $x5->setPower($i);
-        $x5->setTransparent(true);
+        $x5->setTransparent(false);
         $x5->save();
     }
 

fa784ff00db7aa9c7e76881040fafe95a3bb83c5
ACJ <gitlab@acjs.net>
2024-08-03 19:54:35 +0200

    Add SVG versions of static algorithmic characters (up to n=3)
    Refs #8.

---
+++ generator/algos.php
@@ -0,0 +1,25 @@
+<?php
+
+include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
+
+require_once '../src/X5.php';
+require_once '../src/X5Vector.php';
+
+$json = file_get_contents(dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'set.json');
+$set = json_decode($json, true);
+$static = array_diff($set['algorithmic'], $set['dynamic']);
+
+for($i = 1; $i <= 3; $i++) {
+    foreach ($static as $key => $value) {
+        $x5 = new \Acj\X5\X5Vector($value);
+        $x5->setPower($i);
+        $x5->setTransparent(false);
+
+        if(isset($_GET['save'])) {
+            $x5->save();
+            echo 'Saved: ';
+            echo $x5->filename;
+            echo '<br>';
+        }
+    }
+}

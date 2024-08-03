94b7b83533ceee066a27cbde6b2ece630b4a043a
ACJ <gitlab@acjs.net>
2024-08-03 19:37:02 +0200

    Add SVG versions of special characters (up to n=3)
    Refs #8.

--- generator/specials.php
+++ generator/specials.php
@@ -1,14 +1,17 @@
 <?php
 
+include $_composer_autoload_path ?? __DIR__ . '/../vendor/autoload.php';
+
 require_once '../src/X5.php';
+require_once '../src/X5Vector.php';
 
 $specials = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'specials.php';
 
-for($i = 1; $i <= 5; $i++) {
+for($i = 1; $i <= 3; $i++) {
     foreach ($specials as $key => $value) {
-        $x5 = new \Acj\X5\X5($key);
+        $x5 = new \Acj\X5\X5Vector($key);
         $x5->setPower($i);
-        $x5->setTransparent(true);
+        $x5->setTransparent(false);
 
         if(isset($_GET['save'])) {
             $x5->save();

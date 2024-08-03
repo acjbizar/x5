b3caeac8fde217135321793570bfc3275f3cadbc
ACJ <gitlab@acjs.net>
2024-08-03 20:36:44 +0200

    Add SVG versions with transparent background for all Unicode characters (up to `n = 3`)
    Refs #8.

--- generator/chars-svg.php
+++ generator/chars-svg.php
@@ -9,10 +9,10 @@ require_once '../src/X5Vector.php';
 
 $chars = include dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'chars.php';
 
-for($i = 4; $i <= 4; $i++) {
+for($i = 1; $i <= 3; $i++) {
     foreach ($chars as $key => $value) {
         $x5 = new \Acj\X5\X5Vector($key);
-        $x5->setTransparent(false);
+        $x5->setTransparent(true);
         $x5->setPower($i);
 
         if(isset($_GET['save'])) {
